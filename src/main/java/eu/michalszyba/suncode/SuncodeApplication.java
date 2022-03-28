75dddf431d681d9893098cf370c9d2968bfc984e
Michał Szyba <m.szyba@gmail.com>
2022-03-28 09:52:29 +0200

    Initial commit

---
+++ src/main/java/eu/michalszyba/suncode/SuncodeApplication.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.suncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuncodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuncodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/michalszyba/suncode/SuncodeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.suncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuncodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
